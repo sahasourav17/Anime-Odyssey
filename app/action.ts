b83087dc5eee7178d4702f4825d91a52f333b9d3
sahasourav17 <sahasourav1170@gmail.com>
2024-04-23 11:01:39 +0600

    feat: add server actions

---
+++ app/action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+export const fetchAnime = async (page: Number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+  const data = await response.json();
+  return data;
+};
